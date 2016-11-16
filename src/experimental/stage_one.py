329c626bfe1ee8dfe6b572fef9c0b8515efd1718
shelbyt <sht005@eng.ucsd.edu>
2016-11-16 12:51:22 -0800

    Clean up

--- src/experimental/stage_one.py
+++ src/experimental/stage_one.py
@@ -28,45 +28,95 @@ from __future__ import unicode_literals
 import youtube_dl
 import boto3
 import os
+import sys
+import subprocess
+
+class Element:
+    def __init__(self,name):
+        self.name = name
 
 class Logger(object):
-        def debug(self, msg):
-                pass
+    def debug(self, msg):
+            pass
 
-        def warning(self, msg):
-                print(msg)
+    def warning(self, msg):
+            print(msg)
 
-        def error(self, msg):
-                print(msg)
+    def error(self, msg):
+            print(msg)
 
 client = boto3.client('s3')
-bucket = 'shelby-lambda-out'
+#bucket = 'shelby-lambda-out'
 
-def upload(d):
-        filename = "{0}.webm".format(str(d['id']))
+# Generic file uploader. Given the bucket name and file
+# the contents are uploaded.
+def upload(d, bucket, optional=0):
+    if optional == 0:
+        filename = "{0}.mp4".format(str(d['id']))
         filepath = "/tmp/{0}".format(filename)
         client.upload_file(filepath, bucket, filename)
         client.put_object_acl(ACL='public-read', Bucket=bucket, Key=filename)
         region = "us-west-2"
         return "https://s3-%s.amazonaws.com/{0}/{1}".format(region, bucket, filename)
+    if optional == 1:
+        for root, dirs, files in os.walk('/tmp/frames/'):
+            for file in files:
+                print (file)
+                client.upload_file(os.path.join(root, file), bucket, file)
+                client.put_object_acl(ACL='public-read', Bucket=bucket, Key=file)
+        region = "us-west-2"
+        return "https://s3-%s.amazonaws.com/{0}".format(region, bucket)
 
 def processing_hook(d):
-        if d['status'] == 'finished':
-                print('Done downloading, now converting ...')
+    if d['status'] == 'finished':
+            print('Done downloading, now converting ...')
 
-def run(event, context):
+def split_video(d):
+    print ('splitting video start')
+    filename = "{0}.mp4".format(str(d['id']))
+    filepath = "/tmp/{0}".format(filename)
+    split_cmd = 'ffmpeg -i %s'  % (filepath)
+    split_cmd += ' -vf fps=1/10 /tmp/frames/%05d.png'
+    print (split_cmd)
+    output = subprocess.Popen(split_cmd, shell = True, stdout =
+            subprocess.PIPE).stdout.read()
+
+    #print subprocess.check_output(["ls, /tmp/"])
+    #print subprocess.check_output(["ls, /tmp/frames"])
+    print ('splitting video end')
+    print(output)
+    
+
+# Grab and download a video from a variety of sources
+# Problem: What if the video is >500mb?
+def load_video(event):
+    # source 0 = from s3 bucket
+    # source 1 = from url
+    if int(event['source']) is 1:
+        print ('shelby START\n')
         url = event['url']
         ydl_opts = {
                 'outtmpl': '/tmp/%(id)s.%(ext)s',
-                'format': 'bestaudio/best',
+                'format': 'bestvideo/best',
                 'logger': Logger(),
                 'progress_hooks': [processing_hook],
         }
-
         ydl = youtube_dl.YoutubeDL(ydl_opts)
         ydl.add_default_info_extractors()
         info = ydl.extract_info(url, True)
-        print info
+        return info
 
-        response = upload(info)
-        return { "url" : response }
+# Generic operation runner. Give it an input and output along with
+# the binary you want to run and options for the binary
+def operation(in_loc, out_loc, binary, options):
+    print test 
+
+
+def run(event, context):
+    # Clear tmp directory
+    subprocess.call(["rm","-rf","/tmp/*"])
+    subprocess.call(["mkdir","/tmp/frames"])
+    info = load_video(event)
+    split_video(info)
+    response = upload(info, event['bucket'])
+    return { "url" : response }
